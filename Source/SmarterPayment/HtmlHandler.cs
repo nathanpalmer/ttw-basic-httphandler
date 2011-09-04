b525935666fa34726289c50ab0b39eef0759b1e1
Nathan Palmer <nate@thinktheweb.com>
2011-09-04 12:02:53 -0400

    added httphandler project

---
+++ Source/SmarterPayment/HtmlHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmarterPayment
+{
+    public class HtmlHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write(@"
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div>
+        Test
+    </div>
+</body>
+</html>
+");
+            context.Response.ContentType = "html";
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
